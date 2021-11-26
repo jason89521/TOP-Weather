e42bb958217c003c2a0b4bede94eda7e3bb16b00
jason89521 <97ssps30212@gmail.com>
2021-11-26 18:34:12 +0800

    Initial Config

---
+++ js/index.js
@@ -0,0 +1,10 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+const a = 0;
+console.log(a);
+/******/ })()
+;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiaW5kZXguanMiLCJtYXBwaW5ncyI6Ijs7Ozs7QUFBQTtBQUNBLGUiLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly90b3Atd2VhdGhlci8uL3NyYy9pbmRleC5qcyJdLCJzb3VyY2VzQ29udGVudCI6WyJjb25zdCBhID0gMDtcbmNvbnNvbGUubG9nKGEpOyJdLCJuYW1lcyI6W10sInNvdXJjZVJvb3QiOiIifQ==
